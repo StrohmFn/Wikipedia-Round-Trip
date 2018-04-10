10e1b2e93677619078e25e3e8291cf72d3e54a18
strohmfn <florian.strohm@outlook.com>
2018-04-10 10:38:52 +0200

    added wikipedia api requests

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,65 +1,78 @@
+var map = L.map('leafletMap');
 
+map.setView([ 48.745577, 9.1065019369 ], 6);
+L
+		.tileLayer(
+				'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+				{
+					attribution : 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+					maxZoom : 18
+				}).addTo(map);
 
-					var map = L.map('leafletMap');
+var marker;
 
-					map.setView([ 48.745577, 9.1065019369 ], 6);
-					L
-							.tileLayer(
-									'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
-									{
-										attribution : 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-										maxZoom : 18
-									}).addTo(map);
+map.locate({
+	setView : true
+}).on('locationfound', function(e) {
+	marker = new L.marker(e.latlng, {
+		draggable : true
+	});
+	// marker.bindPopup(e.latitude + " " + e.longitude);
+	map.addLayer(marker);
+})
 
-					var marker;
+map.on('click', function(e) {
+	console.log(e.latlng);
+	if (typeof (marker) === 'undefined') {
+		marker = new L.marker(e.latlng, {
+			draggable : true
+		});
+		marker.addTo(map);
+	} else {
+		marker.setLatLng(e.latlng);
+	}
 
-					map.locate({
-						setView : true
-					}).on('locationfound', function(e) {
-						marker = new L.marker(e.latlng, {
-							draggable : true
-						});
-						// marker.bindPopup(e.latitude + " " + e.longitude);
-						map.addLayer(marker);
-					})
+});
 
-					map.on('click', function(e) {
-						console.log(e.latlng);
-						if (typeof (marker) === 'undefined') {
-							marker = new L.marker(e.latlng, {
-								draggable : true
-							});
-							marker.addTo(map);
-						} else {
-							marker.setLatLng(e.latlng);
-						}
+function btnWikiClick() {
+	var lat = marker.getLatLng().lat;
+	var lng = marker.getLatLng().lng;
+	$
+			.ajax({
+				type : "GET",
+				url : "https://de.wikipedia.org/w/api.php?action=query&prop=pageviews&list=geosearch&gsradius=10000&gscoord="
+						+ lat + "%7C" + lng + "&format=json",
+				success : function(response) {
+					// split the response-string into a path
+					console.log(response)
+				}
+			});
+}
 
-					});
-
-					function btnCalculateClick() {
-						var latlngs;
-						var lat = marker.getLatLng().lat;
-						var lng = marker.getLatLng().lng;
-						var startNode = [ lat, lng ];
-						var roundTripDuration = document
-								.querySelector("#duration").value;
-						var visitNodesLat = [ 1, 3, 5 ];
-						var visitNodesLng = [ 2, 4, 6 ];
-						var roundTripPath;
-						var urlString = "/calc/" + startNode + "/"
-								+ roundTripDuration + "/" + visitNodesLat + "/"
-								+ visitNodesLng;
-						$.ajax({
-							type : "GET",
-							url : urlString,
-							async : false,
-							success : function(response) {
-		                        //split the response-string into a path
-		                        latlngs = response.split(",").map(function (e) {
-		                            return e.split("_").map(Number);
-		                        });
-		                        var polyline = L.polyline(latlngs, {color: 'red'}).addTo(map);
-		                        map.fitBounds(polyline.getBounds());
-							}
-						});
-					}
+function btnCalculateClick() {
+	var latlngs;
+	var lat = marker.getLatLng().lat;
+	var lng = marker.getLatLng().lng;
+	var startNode = [ lat, lng ];
+	var roundTripDuration = document.querySelector("#duration").value;
+	var visitNodesLat = [ 1, 3, 5 ];
+	var visitNodesLng = [ 2, 4, 6 ];
+	var roundTripPath;
+	var urlString = "/calc/" + startNode + "/" + roundTripDuration + "/"
+			+ visitNodesLat + "/" + visitNodesLng;
+	$.ajax({
+		type : "GET",
+		url : urlString,
+		async : false,
+		success : function(response) {
+			// split the response-string into a path
+			latlngs = response.split(",").map(function(e) {
+				return e.split("_").map(Number);
+			});
+			var polyline = L.polyline(latlngs, {
+				color : 'red'
+			}).addTo(map);
+			map.fitBounds(polyline.getBounds());
+		}
+	});
+}
